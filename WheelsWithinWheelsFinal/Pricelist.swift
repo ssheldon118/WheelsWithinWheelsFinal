654ce101ff96f6d22d7a0bf2f6c34977bd5c33be
Stephen Sheldon <stephen.sheldon.940@facebook.com>
2019-02-05 10:48:43 -0500

    stevie commit 2

--- WheelsWithinWheelsFinal/Pricelist.swift
+++ WheelsWithinWheelsFinal/Pricelist.swift
@@ -15,25 +15,55 @@ enum TuneUp : String{
     case platinum
 }
 
-class Bike{
-    var name: String
+class RepairPrice{
+    var brand: String
+    var level: TuneUp
+    var price: Double
+    var daysToRepair: Int
     
-    var silver: Double
-    var gold: Double
-    var platinum: Double
     
-    var type: TuneUp?
-    
-    init(_ name: String, _ silver: Double, _ gold: Double, _ platinum: Double){
-        self.name = name
-        self.silver = silver
-        self.gold = gold
-        self.platinum = silver
+    init(_ brand: String, _ level: TuneUp, _ price: Double, _ days: Int){
+        self.brand = brand
+        self.level = level
+        self.price = price
+        self.daysToRepair = days
     }
 }
 
 
 
-var bikelist: [Bike] = [Bike("Trek", 40, 60, 100), Bike("Cannondale", 40, 60, 100), Bike("Salsa", 50, 70, 115), Bike("Jamis", 60, 75, 120), Bike("Specialized", 50, 70, 120), Bike("Surly", 45, 60, 105), Bike("Giant", 40, 60, 105), Bike("Bianchi", 65, 85, 135), Bike("Soma", 65, 80, 135), Bike("Cervelo", 60, 75, 120)]
+
+
+var bikelist: [RepairPrice] = [RepairPrice("Trek", .silver, 40, 4),
+                               RepairPrice("Cannondale", .silver, 40, 4),
+                               RepairPrice("Salsa", .silver, 50, 4),
+                               RepairPrice("Jamis", .silver, 60, 4),
+                               RepairPrice("Specialized", .silver, 50, 4),
+                               RepairPrice("Surly", .silver, 45, 4),
+                               RepairPrice("Giant", .silver, 40, 4),
+                               RepairPrice("Bianchi", .silver, 65, 4),
+                               RepairPrice("Soma", .silver, 65, 4),
+                               RepairPrice("Cervelo", .silver, 60, 4),
+                               RepairPrice("Trek", .gold, 60, 4),
+                               RepairPrice("Cannondale", .gold, 60, 4),
+                               RepairPrice("Salsa", .gold, 70, 4),
+                               RepairPrice("Jamis", .gold, 75, 4),
+                               RepairPrice("Specialized", .gold, 70, 4),
+                               RepairPrice("Surly", .gold, 60, 4),
+                               RepairPrice("Giant", .gold, 60, 4),
+                               RepairPrice("Bianchi", .gold, 85, 4),
+                               RepairPrice("Soma", .gold, 80, 4),
+                               RepairPrice("Cervelo", .gold, 75, 4),
+                               RepairPrice("Trek", .platinum, 100, 4),
+                               RepairPrice("Cannondale", .platinum, 100, 4),
+                               RepairPrice("Salsa", .platinum, 115, 4),
+                               RepairPrice("Jamis", .platinum, 120, 4),
+                               RepairPrice("Specialized", .platinum, 120, 4),
+                               RepairPrice("Surly", .platinum, 105, 4),
+                               RepairPrice("Giant", .platinum, 105, 4),
+                               RepairPrice("Bianchi", .platinum, 135, 4),
+                               RepairPrice("Soma", .platinum, 135, 4),
+                               RepairPrice("Cervelo", .platinum, 120, 4),
+]
 
 
